c2bdb55a132b2177ac1436e6b4b14568f0cc1b18
Kantheti <pk064816@cerner.net>
2021-02-01 16:57:12 +0530

    Initial commit

---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dictionary do
+  it "has a version number" do
+    expect(Dictionary::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
